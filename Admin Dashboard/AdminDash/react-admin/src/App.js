7b70179db6748b4b6aead981947587c9412d445e
Ashfak Azad Nafi <ashfakazadnafi@gmail.com>
2024-10-10 08:32:00 +0600

    first comm

---
+++ Admin Dashboard/AdminDash/react-admin/src/App.js
@@ -0,0 +1,29 @@
+import { useMode, ColorModeContext } from './scenes/theme';
+
+import { CssBaseline, ThemeProvider } from "@mui/material";
+
+import Topbar from "./scenes/dashboard/global/Topbar";
+
+
+
+function App() {
+  const [theme, colorMode] = useMode();
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+            <Topbar/>
+            
+
+          </main>
+          
+          </div>;
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+}
+
+export default App;
